62ecaf6b8945e52f0993bd39c85209b1ed99b8ce
Romastyi83 <49551962+Romastyi83@users.noreply.github.com>
2023-01-20 13:05:11 +0300

    Add files via upload

---
+++ app/js/components/main.js
@@ -0,0 +1,79 @@
+const swiper = new Swiper('.swiper', {
+  loop: true,
+  slidesPerView: 3,
+  spaceBetween: 47,
+  centeredSlides: true,
+  centerInsufficientSlides: true,
+  centeredSlidesBounds: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+const topSwiper = new Swiper('.top-swiper', {
+  loop: true,
+  slidesPerView: 1,
+  allowTouchMove: true,
+  autoplay: {
+    delay: 5000,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    verticalClass: 'swiper-pagination-vertical',
+    clickable: true,
+  },
+});
+
+$('[data-pencil]').on('click', function () {
+  $('[data-popup]').removeClass('active')
+  $(this).next().addClass('active')
+})
+
+$(document).click(function (e) {
+  if (!$('[data-pencil]').is(e.target)
+    && $('[data-pencil]').has(e.target).length === 0
+    && !$('[data-popup]').is(e.target)
+    && $('[data-popup]').has(e.target).length === 0) {
+    $('[data-popup]').removeClass('active')
+  }
+});
+
+$('[data-close]').on('click', function () {
+  $('[data-popup]').removeClass('active')
+})
+
+$('[data-control-center]').on('click', function () {
+  $('[data-popup]').removeClass('active')
+  $('[data-settings-control-center]').addClass('active')
+})
+
+$(document).click(function (e) {
+  if (!$('[data-control-center]').is(e.target)
+    && $('[data-control-center]').has(e.target).length === 0
+    && !$('[data-settings-control-center]').is(e.target)
+    && $('[data-settings-control-center]').has(e.target).length === 0) {
+    $('[data-settings-control-center]').removeClass('active')
+  }
+});
+
+$('[data-close-control-center]').on('click', function () {
+  $('[data-settings-control-center]').removeClass('active')
+})
+
+$('[data-show-products]').on('click', function (event) {
+  event.preventDefault();
+  if ($('[data-more-products]').hasClass('hidden')) {
+    $('[data-more-products]').slideDown();
+    $('[data-show-products]').text('Скрыть')
+    $('[data-more-products]').removeClass('hidden')
+  } else {
+    $('[data-more-products]').slideUp();
+    $('[data-show-products]').text('показать другие')
+    $('[data-more-products]').addClass('hidden')
+  }
+})
+
+$('.btn__dots-wrapper').on('click', function () {
+  $(this).children(":first").toggleClass('btn__dots-inner--slider')
+})
