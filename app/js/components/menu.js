131907b31c6f6c4ec07209202ce01c8ff7d2637f
Roman <romastyi83@gmail.com>
2023-04-20 18:39:42 +0300

    menu

--- app/js/components/menu.js
+++ app/js/components/menu.js
@@ -1,5 +1,5 @@
 $('[data-menu-btn]').click(function () {
   $('[data-logo-text]').toggle()
   $('[data-menu]').toggleClass('active')
-  $('data-hide-menu').toggle()
+  $('[data-menu-hide]').toggle()
 })
