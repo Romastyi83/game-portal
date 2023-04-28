c955ec9b0d9e004b4b3cf7ad920de9ec9dbbaa5d
Roman <romastyi83@gmail.com>
2023-04-28 19:20:51 +0300

    add slider on recall

---
+++ app/js/components/settings-news-slider.js
@@ -0,0 +1,73 @@
+
+const settingsNewsSwiper = new Swiper('[data-news-slider]', {
+  slidesPerView: 2,
+  allowTouchMove: true,
+  navigation: {
+    nextEl: '[data-news-slider-next]',
+    prevEl: '[data-news-slider-prev]',
+  },
+  pagination: {
+    el: '[data-news-slider-pagination]',
+    clickable: true,
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 27,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 27,
+    },
+    1250: {
+      slidesPerView: 2,
+      spaceBetween: 27,
+    },
+  },
+});
+
+const initializationDelNewsSlide = function () {
+  $('[data-del-news-slide]').each((index, delSlide) => {
+    $(delSlide).click(function () {
+      if ($('[data-news-slider] .swiper-slide').length === 1) {
+        return
+      }
+      setTimeout(function () {
+        $(delSlide).closest('.swiper-slide').remove();
+        settingsNewsSwiper.update();
+        writeCounterNewsSlides();
+      }, 100);
+    });
+  })
+}
+
+initializationDelNewsSlide();
+
+$('[data-add-news-slide]').each((index, addSlide) => {
+  $(addSlide).click(function () {
+    $('[data-news-slider] .swiper-slide:last-child').clone().appendTo($('[data-news-slider] .swiper-wrapper'));
+    settingsNewsSwiper.update();
+    initializationDelNewsSlide();
+    writeCounterNewsSlides();
+  });
+})
+
+$('[data-counter-min-news-slides]').click(function () {
+  if (+($('[data-counter-news-slides]').text()) <= 1) {
+    return
+  }
+  $('[data-counter-news-slides]').text(+($('[data-counter-news-slides]').text()) - 1);
+});
+
+const writeCounterNewsSlides = function () {
+  $('[data-counter-news-slides]').text($('[data-news-slider] .swiper-slide').length);
+}
+
+writeCounterNewsSlides();
+
+$('[data-counter-max-news-slides]').click(function () {
+  if (+($('[data-counter-news-slides]').text()) >= $('[data-news-slider] .swiper-slide').length) {
+    return
+  }
+  $('[data-counter-news-slides]').text(+($('[data-counter-news-slides]').text()) + 1);
+});
---
+++ app/js/components/settings-recall-slider.js
@@ -0,0 +1,73 @@
+
+const settingsRecallSwiper = new Swiper('[data-recall-slider]', {
+  slidesPerView: 2,
+  allowTouchMove: true,
+  navigation: {
+    nextEl: '[data-recall-slider-next]',
+    prevEl: '[data-recall-slider-prev]',
+  },
+  pagination: {
+    el: '[data-recall-slider-pagination]',
+    clickable: true,
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 27,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 27,
+    },
+    1250: {
+      slidesPerView: 2,
+      spaceBetween: 27,
+    },
+  },
+});
+
+const initializationDelRecallSlide = function () {
+  $('[data-del-recall-slide]').each((index, delSlide) => {
+    $(delSlide).click(function () {
+      if ($('[data-recall-slider] .swiper-slide').length === 1) {
+        return
+      }
+      setTimeout(function () {
+        $(delSlide).closest('.swiper-slide').remove();
+        settingsRecallSwiper.update();
+        writeCounterRecallSlides();
+      }, 100);
+    });
+  })
+}
+
+initializationDelRecallSlide();
+
+$('[data-add-recall-slide]').each((index, addSlide) => {
+  $(addSlide).click(function () {
+    $('[data-recall-slider] .swiper-slide:last-child').clone().appendTo($('[data-recall-slider] .swiper-wrapper'));
+    settingsRecallSwiper.update();
+    initializationDelRecallSlide();
+    writeCounterRecallSlides();
+  });
+})
+
+$('[data-counter-min-recall-slides]').click(function () {
+  if (+($('[data-counter-recall-slides]').text()) <= 1) {
+    return
+  }
+  $('[data-counter-recall-slides]').text(+($('[data-counter-recall-slides]').text()) - 1);
+});
+
+const writeCounterRecallSlides = function () {
+  $('[data-counter-recall-slides]').text($('[data-recall-slider] .swiper-slide').length);
+}
+
+writeCounterRecallSlides();
+
+$('[data-counter-max-recall-slides]').click(function () {
+  if (+($('[data-counter-recall-slides]').text()) >= $('[data-recall-slider] .swiper-slide').length) {
+    return
+  }
+  $('[data-counter-recall-slides]').text(+($('[data-counter-recall-slides]').text()) + 1);
+});
